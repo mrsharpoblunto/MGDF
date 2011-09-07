d3b2c36685d13f4def813dd75fe849480bf0cb93
Glenn Conner <mr.sharpoblunto@gmail.com>
2011-09-08 00:16:32 +1200

    added missing openal dll.

--- Matchstick/automation/deployLibs.bat
+++ Matchstick/automation/deployLibs.bat
@@ -7,6 +7,7 @@ XCOPY  ..\vendor\lib\%1\xerces-c_2_7.dll bin\%1 /E /Y /I
 echo copying MGDF precompiled dll's to bin directory
 XCOPY  ..\vendor\lib\Release\GameuxInstallHelper.dll bin\%1 /E /Y /I
 XCOPY  ..\vendor\lib\Release\OpenAL32.dll bin\%1 /E /Y /I
+XCOPY  ..\vendor\lib\Release\wrap_oal.dll bin\%1 /E /Y /I
 XCOPY  ..\vendor\lib\Release\alut.dll bin\%1 /E /Y /I
 XCOPY  ..\vendor\lib\Release\ogg.dll bin\%1 /E /Y /I
 XCOPY  ..\vendor\lib\Release\vorbis.dll bin\%1 /E /Y /I
