85920a6495b99296d20414cb33764d86fab57d6d
Glenn Conner <mr.sharpoblunto@gmail.com>
2012-11-26 16:40:55 -0800

    Now compiling zlib as a static library. Fixed deploy script. Tidied up compiler flags and linker settings for MGDF and lib projects.

--- Matchstick/automation/deployLibs.bat
+++ Matchstick/automation/deployLibs.bat
@@ -2,16 +2,14 @@ mkdir bin\%1
 mkdir bin\%1\%2
 
 echo copying MGDF library dll's to bin directory
-XCOPY  ..\vendor\lib\%1\%2\zlib.dll bin\%1\%2 /E /Y /I
-XCOPY  ..\vendor\lib\%1\%2\fw1FontWrapper.dll bin\%1\%2 /E /Y /I
+XCOPY  ..\vendor\lib\%1\%2\alut.dll bin\%1\%2 /E /Y /I
+XCOPY  ..\vendor\lib\%1\%2\ogg.dll bin\%1\%2 /E /Y /I
+XCOPY  ..\vendor\lib\%1\%2\vorbis.dll bin\%1\%2 /E /Y /I
+XCOPY  ..\vendor\lib\%1\%2\vorbisfile.dll bin\%1\%2 /E /Y /I
 
 echo copying MGDF precompiled dll's to bin directory
 XCOPY  ..\vendor\lib\Release\GameuxInstallHelper.dll bin\%1\%2 /E /Y /I
 XCOPY  ..\vendor\lib\%1\Release\OpenAL32.dll bin\%1\%2 /E /Y /I
 XCOPY  ..\vendor\lib\%1\Release\wrap_oal.dll bin\%1\%2 /E /Y /I
-XCOPY  ..\vendor\lib\%1\Release\alut.dll bin\%1\%2 /E /Y /I
-XCOPY  ..\vendor\lib\%1\Release\ogg.dll bin\%1\%2 /E /Y /I
-XCOPY  ..\vendor\lib\%1\Release\vorbis.dll bin\%1\%2 /E /Y /I
-XCOPY  ..\vendor\lib\%1\Release\vorbisfile.dll bin\%1\%2 /E /Y /I
 
 echo DEPLOY COMPLETED
