4a2da3f614e405da1159e27920bf990005b7b330
Glenn <mr.sharpoblunto@gmail.com>
2014-09-08 20:33:30 -0700

    clearing out docs branch

--- Matchstick/src/GamesManager/GamesManager.StatisticsService.Model/schema.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE dbo.mgdf_statistics (
-  "Id" uniqueidentifier NOT NULL PRIMARY KEY,
-  "GameUid" VarChar(255) NOT NULL,
-  "Name" VarChar(127) NOT NULL,
-  "Value" VarChar(127) NOT NULL
-);
